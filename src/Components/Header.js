75bec9bf3e0b19347b59263979c0319f0b8aa0e2
Andrzej Herman <mail@aherman.org>
2021-10-30 19:24:29 +0200

    30.10.2021

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Katalog filmowy</h1>
+      <p>Aplikacja nr 2 z przedmiotu Wykorzystywanie wzorców w internecie</p>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../Components/Header";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header/>
+      {/* SEARCHBAR */}
+      {/* CATALOG */}
+      {/* FOOTER */}
+    </>
+  );
+};
+
+export default HomePage;
